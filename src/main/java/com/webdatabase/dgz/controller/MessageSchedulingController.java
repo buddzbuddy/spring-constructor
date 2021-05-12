19bf7897f28d99e4f1bf9104e5b5aa562785c80d
buddzbuddy <abormokov@yahoo.com>
2021-05-12 12:57:00 +0600

    Quartz scheduler

--- src/main/java/com/webdatabase/dgz/controller/MessageSchedulingController.java
+++ src/main/java/com/webdatabase/dgz/controller/MessageSchedulingController.java
@@ -1,5 +1,8 @@
 package com.webdatabase.dgz.controller;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 //import java.util.Date;
 import java.util.Optional;
 import org.quartz.JobBuilder;
@@ -8,12 +11,20 @@ import org.quartz.JobKey;
 import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
 import org.quartz.Trigger;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.quartz.CronScheduleBuilder;
 import org.quartz.TriggerBuilder;
 import org.quartz.TriggerKey;
+import org.quartz.impl.matchers.GroupMatcher;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,6 +34,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.webdatabase.dgz.dto.MessageDto;
 import com.webdatabase.dgz.job.MessageJob;
 import com.webdatabase.dgz.model.Message;
+import com.webdatabase.dgz.model.MsecDetail;
+import com.webdatabase.dgz.model.Supplier;
+import com.webdatabase.dgz.model.SupplierMember;
 import com.webdatabase.dgz.repository.MessageRepository;
 @RestController
 @RequestMapping(path = "/messages")
@@ -60,7 +74,7 @@ public class MessageSchedulingController {
                     .build();*/
             Trigger trigger = TriggerBuilder.newTrigger()
             .withIdentity(id, "group1")
-            .withSchedule(CronScheduleBuilder.cronSchedule("0/15 * * * * ? *").withMisfireHandlingInstructionFireAndProceed())
+            .withSchedule(CronScheduleBuilder.cronSchedule("0/30 * * * * ? *").withMisfireHandlingInstructionFireAndProceed())
             //.forJob("myJob", "group1")
             .build();
             
@@ -68,6 +82,8 @@ public class MessageSchedulingController {
             Scheduler scheduler = schedulerFactoryBean.getScheduler();
             scheduler.scheduleJob(jobDetail, trigger);
             scheduler.start();
+            
+            
 
             messageDto.setStatus("SUCCESS");
 
@@ -112,4 +128,40 @@ public class MessageSchedulingController {
         }
         return messageDto;
     }
+
+
+
+	@GetMapping(path = "/list-job",
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<?> listJob() throws JSONException {
+		Scheduler scheduler = schedulerFactoryBean.getScheduler();
+		JSONObject response = new JSONObject();
+		response.put("message", "Job list");
+		List<String> items = new ArrayList<String>();
+		   try {
+			for (String groupName : scheduler.getJobGroupNames()) {
+
+			     for (JobKey jobKey : scheduler.getJobKeys(GroupMatcher.jobGroupEquals(groupName))) {
+			                
+			      String jobName = jobKey.getName();
+			      String jobGroup = jobKey.getGroup();
+			                
+			      //get job's trigger
+			      List<Trigger> triggers = (List<Trigger>) scheduler.getTriggersOfJob(jobKey);
+			      Date nextFireTime = triggers.get(0).getNextFireTime(); 
+
+			      String item = "[jobName] : " + jobName + " [groupName] : "
+				            + jobGroup + " - " + nextFireTime;
+			        System.out.println(item);
+			        items.add(item);
+			      }
+			    }
+		} catch (SchedulerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		response.accumulate("list", items);
+		
+		return new ResponseEntity<>(response.toString(), HttpStatus.OK);
+	}
 }
